4c27a495f4bdb4de5b5e53bc46ee756b95309072
Louis Stevens <loustevenshere@gmail.com>
2020-10-07 13:43:33 -0400

    First test done

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ Use the copy function below to do the following:
     2. Return the new array that holds an exact copy of the old array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+function copy (arr) {
+   return [...arr]
+}
 
 
 
